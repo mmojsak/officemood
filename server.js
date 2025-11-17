dffa12a948de28dc884ef384a50aaa7a85cf0372
Mateusz Mojsak <mateuszmojsak@gmail.com>
2025-11-17 20:22:40 +0000

    comments

--- server.js
+++ server.js
@@ -7,22 +7,45 @@ const PORT = 3000;
 app.use(express.static("."));
 app.use(express.json());
 
-// Endpoint to get happiness data
+const DATA_FILE = path.join(__dirname, "data.json");
+
+// ----------------------
+// GET endpoint: send happiness + comments
+// ----------------------
 app.get("/data", (req, res) => {
-    fs.readFile("data.json", "utf8", (err, data) => {
+    fs.readFile(DATA_FILE, "utf8", (err, data) => {
         if (err) return res.status(500).send(err);
-        res.json(JSON.parse(data));
+        let happinessData = JSON.parse(data);
+        // Make sure each item has both happiness and comment
+        happinessData = happinessData.map(item => ({
+            happiness: item.happiness ?? 0,
+            comment: item.comment ?? ""
+        }));
+        res.json(happinessData);
     });
 });
 
-// Endpoint to update happiness data
+// ----------------------
+// POST endpoint: update happiness and comment
+// ----------------------
 app.post("/data", (req, res) => {
-    const { index, value } = req.body;
-    fs.readFile("data.json", "utf8", (err, data) => {
+    const { index, value, comment } = req.body;
+
+    fs.readFile(DATA_FILE, "utf8", (err, data) => {
         if (err) return res.status(500).send(err);
+
         let happinessData = JSON.parse(data);
-        happinessData[index] = value;
-        fs.writeFile("data.json", JSON.stringify(happinessData), err => {
+
+        // Ensure the index exists
+        if (!happinessData[index]) {
+            return res.status(400).json({ status: "error", message: "Invalid index" });
+        }
+
+        // Update happiness and comment
+        happinessData[index].happiness = value;
+        happinessData[index].comment = comment || "";
+
+        fs.writeFile(DATA_FILE, JSON.stringify(happinessData, null, 2), err => {
             if (err) return res.status(500).send(err);
             res.json({ status: "ok" });
         });
